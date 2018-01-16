d2e007262ed5643004bce9454d2028bdd19f7e9a
ZSI2017 <13407121461@163.com>
2018-01-16 19:08:25 +0800

    init

--- dva-quickstart/src/router.js
+++ dva-quickstart/src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route, Switch,Redirect,routerRedux} from 'dva/router';
+import dynamic from "dva/dynamic"
 import IndexPage from './routes/IndexPage';
 import Products from './routes/Products';
 
@@ -9,6 +10,7 @@ function RouterConfig({ history }) {
       <Switch>
         <Route path="/" exact component={IndexPage} />
         <Route path="/products" exact component={Products} />
+
       </Switch>
     </Router>
   );
---
+++ dva-quickstart/src/routes/main.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {Layout,Menu,Breadcrumb,Icon} from "antd";
+const {SubMenu} = Menu;
+const {Head,Content,Footer,Sider} = Layout;
+
+
+const Main = ({}) => {
+   
+}
