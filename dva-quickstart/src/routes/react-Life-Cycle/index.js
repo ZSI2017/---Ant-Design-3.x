af817d6f8ad68edc18fc5ddf3cfe6d993b58e4bb
ZSI2017 <13407121461@163.com>
2018-03-06 18:13:07 +0800

    add life cycle

--- dva-quickstart/src/routes/react-Life-Cycle/index.js
+++ dva-quickstart/src/routes/react-Life-Cycle/index.js
@@ -6,15 +6,26 @@ class LifeCycle extends React.Component {
     this.state = {
       color:"red"
     }
+    this.changeState = this.changeState.bind(this)
     console.log("constructor: React组件的构造函数会在装配之前被调用")
+
+  }
+  changeState(){
+    this.setState({
+      color:"blue"
+    })
 
   }
   componentWillUnMount() {
     console.log("componentWillMount:  在装配发生前被立刻调用");
   }
   render(){
+    console.log("render: ");
     return (
-      <div> react life Cycle </div>
+      <div>
+         <div> {this.state.color} </div>
+         <input type="button" onClick={this.changeState} value="changeState"/>
+      </div>
     )
   }
   componentDidMount(){
@@ -25,6 +36,7 @@ class LifeCycle extends React.Component {
   }
   shouldComponentUpdate () {
     console.log("shouldComponentUpdate: 接受到新的属相或状态，在渲染前被调用，默认返回 true")
+    return true;
   }
   componentWillUpdate() {
     console.log("componentWillUpdate:渲染前被立即调用，不能在这调用 this.setState() ");
